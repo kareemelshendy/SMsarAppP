f7366922e60c03c17d9d2f23d599cf0de40c8074
Kareem ElShendy <49846839+kareemelshendy@users.noreply.github.com>
2021-06-09 15:10:05 +0200

    Update auth.service.ts

--- src/app/login/auth.service.ts
+++ src/app/login/auth.service.ts
@@ -47,7 +47,7 @@ export class AuthService {
 
   logout() {
     this.user.next(null);
-    this.router.navigate(["/login-page"]);
+    this.router.navigate(["/login"]);
     localStorage.removeItem("userData");
   }
   autoLogin() {
